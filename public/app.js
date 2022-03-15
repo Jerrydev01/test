1abdf16c1d2a2fa3fa76a2b57e9a4c668d350feb
Zeus012345 <36851589+Zeus012345@users.noreply.github.com>
2022-03-15 14:28:44 +0100

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,13 @@
+// swiper js header
+
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    effect: "fade",
+    autoplay: {
+
+        delay: 3500,
+
+        disableOnInteraction: false
+
+    }
+});
